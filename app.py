80cb978df42cbd77fb52113a88e524b53d8be326
Liviu Brinza <liviu.brinza@mtrail.ch>
2023-02-08 22:11:50 +0200

    templating enhancements

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from flask_bootstrap import Bootstrap4
 from flask import Flask, render_template
 from CsvReader import CsvReader
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap4(app)
+
 @app.route("/")
 def main():
-    return "<h1>Main page</h1>"
+    return render_template('main.html')
 
 @app.route('/products')
 def get_products_list():
@@ -13,3 +16,7 @@ def get_products_list():
     products = csvReader.get_products_list()
 
     return render_template('products.html', products=products)
+
+@app.route('/daily')
+def daily_tracker():
+    return render_template('daily.html')
