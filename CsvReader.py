20405dbf79a1eea5bdfc4f293e1b8214b5b42627
Liviu Brinza <liviu.brinza@mtrail.ch>
2023-02-10 11:35:03 +0200

    add remove products

--- CsvReader.py
+++ CsvReader.py
@@ -2,24 +2,42 @@ import csv
 from Product import Product
 
 class CsvReader:
-        filename = "products.csv"
+    filename = "products.csv"
 
-        def __init__(self):
-            self.products = []
+    def __init__(self):
+        self.products = []
 
-            with open(CsvReader.filename) as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                line_count = 0
+        with open(CsvReader.filename) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            line_count = 0
 
-                for row in reader:
-                    if line_count > 0 and row:
-                        product = Product(name=row[0],
-                                          calories=row[1],
-                                          protein=row[2],
-                                          fats=row[3],
-                                          carbs=row[4])
-                        self.products.append(product)
-                    line_count += 1
+            for row in reader:
+                if line_count > 0 and row:
+                    product = Product(name=row[0],
+                                        calories=row[1],
+                                        protein=row[2],
+                                        fats=row[3],
+                                        carbs=row[4])
+                    self.products.append(product)
+                line_count += 1
 
-        def get_products_list(self):
-            return self.products
+    def get_products_list(self):
+        return self.products
+
+    def add_new_product(self, new_product) :
+        if new_product.name and new_product.calories and new_product.protein and new_product.fats and new_product.carbs:     
+            self.products.append(new_product)
+            print("[INFO] New product added")
+            return True
+        else:
+            print("[ERROR] New product not complete!")
+            return False
+
+    def remove_product(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+                print("[INFO] Product removed")
+                return True
+        print("[ERROR] Product not found to remove")
+        return False
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask_bootstrap import Bootstrap4
 from flask import Flask, render_template, redirect, url_for, request
 from CsvReader import CsvReader
 from DailyTracker import DailyTracker
+from Product import Product
 
 app = Flask(__name__)
 
@@ -22,8 +23,13 @@ def main():
 
 @app.route('/products')
 def get_products_list():
+    message = None
+    if request.args and request.args['message']:
+        print("PRODUCT LIST MESSAGE: " + request.args['message'])
+        message = request.args['message']
+
     products = csvReader.get_products_list()
-    return render_template('products.html', products=products)
+    return render_template('products.html', products=products, message=message)
 
 @app.route('/daily')
 def daily_tracker():
@@ -40,13 +46,39 @@ def daily_tracker():
                             max=17000, 
                             set=zip(values, labels, colors))
 
-@app.route('/add_product', methods=['GET', 'PUT'])
+@app.route('/add_product', methods=['POST'])
 def add_product():
-    print("ADD ITEM ENDPOINT")
-    return redirect(url_for('get_products_list'))
+    print("FORM")
+    print(request.form)
+    args = request.form
+
+    new_product = Product(name=args['name'],
+                          calories=args['calories'],
+                          protein=args['protein'],
+                          fats=args['fats'],
+                          carbs=args['carbs'])
+
+    retVal = csvReader.add_new_product(new_product=new_product)
+    
+    message = None
+    if retVal:
+        message = "Successfully added product: " + args['name']
+    else:
+        message = "Error encountered while adding product: " + args['name']
+
+    return redirect(url_for('get_products_list', message=message))
 
 @app.route('/remove_product', methods=['GET', 'DELETE'])
 def remove_product():
     args = request.args
-    print("REMOVE ITEM ENDPOINT: " + str(args))
-    return redirect(url_for('get_products_list'))
+    name = args['name']
+
+    retVal = csvReader.remove_product(product_name=name)
+
+    message = None
+    if retVal:
+        message = "Successfully removed product: " + name
+    else:
+        message = "Error encountered while removing produc: " + name
+
+    return redirect(url_for('get_products_list', message=message))
